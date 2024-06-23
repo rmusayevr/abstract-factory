7549d71b9788d458b9c26e9da9a5809058e3f8f0
Rashad Musayev <rmusayevr19@gmail.com>
2024-06-23 17:39:39 +0400

    feat: task 11

--- src/pytemplate/entrypoints/cli/main.py
+++ src/pytemplate/entrypoints/cli/main.py
@@ -1,4 +1,4 @@
-from typing import Dict
+from typing import Dict, List
 
 
 def get_city_input() -> Dict:
@@ -6,3 +6,16 @@ def get_city_input() -> Dict:
     country = input("Enter country: ")
     population = int(input("Enter population: "))
     return {"name": name, "country": country, "population": population}
+
+
+def get_dish_input() -> List[Dict]:
+    dishes = []
+    while True:
+        dish_name = input("Enter dish name: ")
+        dish_price = float(input("Enter dish price: "))
+        dishes.append({"name": dish_name, "price": dish_price})
+
+        decide = input("Do you want to stop? (yes/no): ").strip().lower()
+        if decide == "yes":
+            break
+    return dishes
--- tests/test_inputs.py
+++ tests/test_inputs.py
@@ -1,10 +1,17 @@
 from unittest.mock import patch
 
-from src.pytemplate.entrypoints.cli.main import get_city_input
+from src.pytemplate.entrypoints.cli.main import get_city_input, get_dish_input
 
 
 def test_get_city_input():
     with patch("builtins.input", side_effect=["Paris", "France", "2140526"]):
         result = get_city_input()
         expected = {"name": "Paris", "country": "France", "population": 2140526}
+        assert result == expected
+
+
+def test_get_dish_input():
+    with patch("builtins.input", side_effect=["Spaghetti Carbonara", "15.5", "no", "Margherita Pizza", "12.0", "yes"]):
+        result = get_dish_input()
+        expected = [{"name": "Spaghetti Carbonara", "price": 15.5}, {"name": "Margherita Pizza", "price": 12.0}]
         assert result == expected
