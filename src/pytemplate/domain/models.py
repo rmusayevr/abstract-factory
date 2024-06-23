365fdd8ede0207a548ec77941226a67e179fd7bb
Rashad Musayev <rmusayevr19@gmail.com>
2024-06-23 13:30:23 +0400

    feat: task 2

--- src/pytemplate/domain/models.py
+++ src/pytemplate/domain/models.py
@@ -6,3 +6,7 @@ class City:
     name: str
     country: str
     population: int
+
+
+def city_factory(name: str, country: str, population: int) -> City:
+    return City(name=name, country=country, population=population)
--- tests/test_models.py
+++ tests/test_models.py
@@ -1,4 +1,4 @@
-from src.pytemplate.domain.models import City
+from src.pytemplate.domain.models import City, city_factory
 
 
 def test_city_creation():
@@ -6,3 +6,11 @@ def test_city_creation():
     assert city.name == "Paris"
     assert city.country == "France"
     assert city.population == 2148327
+
+
+def test_city_factory():
+    city = city_factory(name="Tokyo", country="Japan", population=13929286)
+    assert isinstance(city, City)
+    assert city.name == "Tokyo"
+    assert city.country == "Japan"
+    assert city.population == 13929286
